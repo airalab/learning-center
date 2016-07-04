026a78d0dcabf0b5133ff2665e1385cf378d133d
vol4tim <vol4tim@gmail.com>
2016-07-04 14:38:56 +0400

    Upd contracts lessson

--- sol/OwnerAir.sol
+++ sol/OwnerAir.sol
@@ -1,10 +1,19 @@
+import '../common/Owned.sol';
 import '../token/TokenEmission.sol';
 
-contract OwnerAir {
+contract OwnerAir is Owned {
     
     TokenEmission public token;
     
     function OwnerAir(address _token) {
+        token = TokenEmission(_token);
+    }
+	
+    function delegateToken(address _owner) onlyOwner {
+        token.delegate(_owner);
+    }
+	
+    function changeToken(address _token) onlyOwner {
         token = TokenEmission(_token);
     }
     
